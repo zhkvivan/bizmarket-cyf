486ae657fb4dbc3db320125676de174fd840d411
Peter Armstrong <peterarmstrong999@gmail.com>
2022-01-24 12:33:23 +0000

    Server running locally and schema.sql made

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import db from "./db";
 
 const router = new Router();
 
@@ -6,4 +7,15 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
+
+
+router.get("/health", (_, res) => {
+	db.query("select version()")
+		.then((result) => {
+			if(result.rows.length > 0) {
+				res.status(200).send({ server:result.rows[0] });
+			}
+		});
+
+});
 export default router;
