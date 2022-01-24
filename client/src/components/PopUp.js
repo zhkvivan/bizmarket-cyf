cd022719d6338d29fadb675d90ba91efd2f993f6
Douglas Maxton <douglasmaxton@gmail.com>
2022-01-24 23:18:29 +0200

    Styled input tag at ends to be rounded

--- client/src/components/PopUp.js
+++ client/src/components/PopUp.js
@@ -1,9 +1,16 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "./Context/Context";
 import "./PopUp.css";
 
 const PopUp = () => {
 	const popUp = useContext(Context);
+	const [text, setText] = useState({
+		name: "",
+		password: "",
+	});
+
+	console.log("text", text);
+
 	return (
 		<>
 			<div className="popup-box">
@@ -12,6 +19,38 @@ const PopUp = () => {
 						X
 					</span>
 					<b>Login into BizMarket</b>
+					<form>
+						<div>
+							<label>Name:</label>
+							<br />
+							<input
+								type="text"
+								name="name"
+								className="input-tag"
+								onChange={(e) =>
+									setText({
+										...text,
+										name: e.target.value,
+									})
+								}
+							/>
+						</div>
+						<div>
+							<label>Password:</label>
+							<br />
+							<input
+								type="password"
+								name="password"
+								className="input-tag"
+								onChange={(e) =>
+									setText({
+										...text,
+										password: e.target.value,
+									})
+								}
+							/>
+						</div>
+					</form>
 				</div>
 			</div>
 		</>
