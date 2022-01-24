56ea0a8f69634d6372a06a1d5dd13f55fa40ba8a
Douglas Maxton <douglasmaxton@gmail.com>
2022-01-24 22:55:28 +0200

    Create a title for popUp form

--- client/src/components/PopUp.js
+++ client/src/components/PopUp.js
@@ -1,7 +1,19 @@
-const PopUp = ({ props }) => {
+import { useContext } from "react";
+import { Context } from "./Context/Context";
+import "./PopUp.css";
+
+const PopUp = () => {
+	const popUp = useContext(Context);
 	return (
 		<>
-			<h1>BizMarket</h1>
+			<div className="popup-box">
+				<div className="box">
+					<span className="close-icon" onClick={popUp.togglePopUp}>
+						X
+					</span>
+					<b>Login into BizMarket</b>
+				</div>
+			</div>
 		</>
 	);
 };
