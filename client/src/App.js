56e7f7f6f68f8744164377baee06535c43278b04
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2022-01-25 06:22:22 +0000

    Hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 
 function App() {
 	return (
 		<>
 			<Routes>{/* <Route path="/" element={} /> */}</Routes>
 			<Header />
+			<Hero/>
 		</>
 	);
 }
