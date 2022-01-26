43bfe3597079f7a1a6c82f13fa280b396900a1b0
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2022-01-26 07:20:15 +0000

    rectified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Layout from "./components/Layout";
+// import About from "./pages/About";
+// import Layout from "./components/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home.js";
 
 function App() {
 	return (
 		<>
-			<Routes>
+			{/* <Routes>
 				<Route path="/" element={<Layout />}>
 					<Route path="/about" element={<About />} />
 				</Route>
-			</Routes>
+			</Routes> */}
+			<Home />
 		</>
 	);
 }
