352b269261b7369580a1405ae7d82d8223a5a76b
Peter Armstrong <peterarmstrong999@gmail.com>
2022-01-27 17:40:21 +0000

    db architecture complete

--- server/api.js
+++ server/api.js
@@ -8,16 +8,13 @@ router.get("/", (_, res) => {
 });
 
 router.get("/health", (_, res) => {
-	db.query("select version()")
-		.then((result) => {
-			if(result.rows.length > 0) {
-				res.status(200).send({ server:result.rows[0] });
-			}
-		});
-
+	db.query("select version()").then((result) => {
+		if (result.rows.length > 0) {
+			res.status(200).send({ server: result.rows[0] });
+		}
+	});
 });
 
-router.get();
+//router.get();
 
 export default router;
-
