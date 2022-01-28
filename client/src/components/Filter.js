646c4c64ac9fa5090541fbc60a62fe08fe915f74
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2022-01-28 15:52:47 +0000

    filter files

---
+++ client/src/components/Filter.js
@@ -0,0 +1,5 @@
+import React from "react";
+import SelectCategory from "./SelectCategory";
+import "./Filter.css";
+
+
---
+++ client/src/components/SelectCategory.js
@@ -0,0 +1,5 @@
+import "./Filter.css";
+
+const SelectCategory = () => <p>Select a category from component</p>
+
+export default SelectCategory;
