bcb234e80308f1b78640cd89ea67384d06d4832c
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2022-02-01 07:20:59 +0000

    error in FilterProduct

--- client/src/components/FilterProduct.js
+++ client/src/components/FilterProduct.js
@@ -1,36 +1,41 @@
 import React from "react";
-// import { Paper, Container, InputBase, IconButton } from "@material-ui/core";
+import { Paper, Container, InputBase, IconButton } from "@material-ui/core";
 
-// import { Search } from "@material-ui/icons";
+import { Search } from "@material-ui/icons";
 
 {
 	/*import SelectCategory from "./SelectCategory";*/
 }
 import "./FilterProduct.scss";
 
-const FilterProduct = () =>{
-<>
-<h1>FilterProduct</h1>
-</>;
-};
-
 // const FilterProduct = () => {
-// 	<div className="filter-bar">
-// 		<Container>
-// 			<Paper component="form" className="root" onSubmit={() => {}}>
-// 				<SelectCategory />
-// 				<InputBase
-// 					className="input"
-// 					onChange={() => {}}
-// 					placeholder="Search for a product"
-// 					inputProps={{ "aria-label": "Search for a product" }}
-// 				/>
-// 				<IconButton type="submit" aria-label="search">
-// 					{/*<Search />)*/}
-// 				</IconButton>
-// 			</Paper>
-// 		</Container>
-// 	</div>;
+// 	return (
+// 		<>
+// 			<h1>FilterProduct</h1>
+// 		</>
+// 	);
 // };
+
+const FilterProduct = () => {
+	return(
+<div className="filter-bar">
+		<Container>
+			<Paper component="form" className="root" onSubmit={() => {}}>
+				<SelectCategory />
+				<InputBase
+					className="input"
+					onChange={() => {}}
+					placeholder="Search for a product"
+					inputProps={{ "aria-label": "Search for a product" }}
+				/>
+				<IconButton type="submit" aria-label="search">
+					{/*<Search />)*/}
+				</IconButton>
+			</Paper>
+		</Container>
+	</div>
+	)
+	
+};
 
 export default FilterProduct;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,13 +3,13 @@ import Banner from "../components/Banner";
 import { Slider } from "../components/Slider";
 import styles from "./Home.module.scss";
 import Hero from "../components/Hero";
-
+import FilterProduct from "../components/FilterProduct";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
-
+			<FilterProduct />
 			<Slider />
 			<Banner />
 		</>
