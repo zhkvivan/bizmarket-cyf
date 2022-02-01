7138b698a72c1d45916a4fe724ca88f839fde76f
Mirabela Butiseaca <43749486+Mira-Bela@users.noreply.github.com>
2022-02-01 07:45:44 +0000

    Search bar is now displaying

--- client/src/components/FilterProduct.js
+++ client/src/components/FilterProduct.js
@@ -3,39 +3,26 @@ import { Paper, Container, InputBase, IconButton } from "@material-ui/core";
 
 import { Search } from "@material-ui/icons";
 
-{
-	/*import SelectCategory from "./SelectCategory";*/
-}
 import "./FilterProduct.scss";
 
-// const FilterProduct = () => {
-// 	return (
-// 		<>
-// 			<h1>FilterProduct</h1>
-// 		</>
-// 	);
-// };
-
 const FilterProduct = () => {
-	return(
-<div className="filter-bar">
-		<Container>
-			<Paper component="form" className="root" onSubmit={() => {}}>
-				<SelectCategory />
-				<InputBase
-					className="input"
-					onChange={() => {}}
-					placeholder="Search for a product"
-					inputProps={{ "aria-label": "Search for a product" }}
-				/>
-				<IconButton type="submit" aria-label="search">
-					{/*<Search />)*/}
-				</IconButton>
-			</Paper>
-		</Container>
-	</div>
-	)
-	
+	return (
+		<div className="filter-bar">
+			<Container>
+				<Paper component="form" className="root" onSubmit={() => {}}>
+					<InputBase
+						className="input"
+						onChange={() => {}}
+						placeholder="Search for a product"
+						inputProps={{ "aria-label": "Search for a product" }}
+					/>
+					<IconButton type="submit" aria-label="search">
+						{/*<Search />)*/}
+					</IconButton>
+				</Paper>
+			</Container>
+		</div>
+	);
 };
 
 export default FilterProduct;
