9e5de5ec6f7724a7118130b108616c1657aa25dc
Peter Armstrong <peterarmstrong999@gmail.com>
2022-02-09 13:53:44 +0000

    category fix

--- server/model/data.sql
+++ server/model/data.sql
@@ -1,13 +1,17 @@
 
-INSERT INTO category(name, link) VALUES('Food, Drinks', 'food-drink');
-INSERT INTO category(name, link) VALUES('Automotive', 'automotive');
-INSERT INTO category(name, link) VALUES('Electronics', 'electronics');
-INSERT INTO category(name, link) VALUES('Fashion, Clothing', 'fashion-clothing');
-INSERT INTO category(name, link) VALUES('Floral, Garden', 'floral-garden');
-INSERT INTO category(name, link) VALUES('Health, Beauty', 'health-beauty');
-INSERT INTO category(name, link) VALUES('Home, Decor', 'home-decor');
-INSERT INTO category(name, link) VALUES('Industrial, Materials', 'industrial-materials');
-INSERT INTO category(name, link) VALUES('Pets, Animals', 'pets-animals');
-INSERT INTO category(name, link) VALUES('Sports, Outdoors', 'sports-outdoors');
-INSERT INTO category(name, link) VALUES('Toys, Games', 'toys-games');
-INSERT INTO category(name, link) VALUES('Other', 'other');
+INSERT INTO category(id, name, link) VALUES('1', 'Food, Drinks', 'food-drink'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('2', 'Automotive', 'automotive'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('3', 'Electronics', 'electronics'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('4', 'Fashion, Clothing', 'fashion-clothing'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('5', 'Fashion, Clothing', 'fashion-clothing'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('6', 'Fashion, Clothing', 'fashion-clothing'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('7', 'Fashion, Clothing', 'fashion-clothing'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('8', 'Fashion, Clothing', 'fashion-clothing'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('9', 'Floral, Garden', 'floral-garden'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('10', 'Health, Beauty', 'health-beauty'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('11', 'Home, Decor', 'home-decor'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('12', 'Industrial, Materials', 'industrial-materials'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('13', 'Pets, Animals', 'pets-animals'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('14', 'Sports, Outdoors', 'sports-outdoors'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('15', 'Toys, Games', 'toys-games'); ON CONFLICT DO NOTHING
+INSERT INTO category(id, name, link) VALUES('16', 'Other', 'other'); ON CONFLICT DO NOTHING
