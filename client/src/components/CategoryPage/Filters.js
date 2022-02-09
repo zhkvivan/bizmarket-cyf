ff133f39f8aed8a4375e4570de066a3597b97456
Douglas Maxton <douglasmaxton@gmail.com>
2022-02-09 08:23:05 +0200

    Created 2 input tags min and max

--- client/src/components/CategoryPage/Filters.js
+++ client/src/components/CategoryPage/Filters.js
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { useContextBM } from "../../context/Context";
 import styles from "./Filters.module.scss";
 
@@ -8,8 +7,26 @@ const Filters = () => {
 	return (
 		<div className={isFilterOpen ? styles["filter-open"] : styles.filters}>
 			<h2>Filters</h2>
+			<input type="number" placeholder="min" />
+			<input type="number" placeholder="max" />
 		</div>
 	);
 };
 
 export default Filters;
+
+// import RAeact, { useState } from "react";
+// import { useContextBM } from "../../context/Context";
+// import styles from "./Filters.module.scss";
+
+// const Filters = () => {
+// 	const { isFilterOpen, setIsFilterOpen } = useContextBM();
+
+// 	return (
+// 		<div className={isFilterOpen ? styles["filter-open"] : styles.filters}>
+// 			<h2>Filters</h2>
+// 		</div>
+// 	);
+// };
+
+// export default Filters;
