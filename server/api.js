4330ef0f27efa184f0b0946ddd57d32e19cf0c80
JubyThomas <jubyroselinethomas@gmail.com>
2022-02-10 12:42:51 +0000

    api to view all ads

--- server/api.js
+++ server/api.js
@@ -5,7 +5,6 @@ import cors from "cors";
 const router = new Router();
 router.use(cors());
 
-
 router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
@@ -19,7 +18,6 @@ router.get("/health", (_, res) => {
 });
 
 router.get("/categories", (_, res) => {
-
 	db.query("SELECT id, name, link FROM category")
 		.then((result) => {
 			console.debug("successfully got all categories", result);
@@ -109,6 +107,24 @@ router.get("/ad", (_, res) => {
 			res
 				.status(500) // Internal server error
 
+				.json({ message: "Oh, no! Something went wrong... Sorry about that!" });
+		});
+});
+
+router.get("/viewads", (_, res) => {
+	db.query(
+		"SELECT adTitle, sellerName, sellerCompany,sellerPhone,sellerEmail,expiryDate,price,quantity,minQuantity,description,location,imageURL FROM adListing INNER JOIN  category ON  adListing.categoryId=category.categoryId"
+	)
+		.then((result) => {
+			console.debug("successfully got all ads", result);
+			if (result.rows.length > 0) {
+				res.status(200).send({ results: result.rows });
+			}
+		})
+		.catch((error) => {
+			console.error("Failed to get all ads", error);
+			res
+				.status(500)
 				.json({ message: "Oh, no! Something went wrong... Sorry about that!" });
 		});
 });
