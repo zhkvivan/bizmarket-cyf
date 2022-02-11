cafba21c102e3d86d1bd0be3854e6d6df46d8034
Peter Armstrong <peterarmstrong999@gmail.com>
2022-02-11 17:28:03 +0000

    data.sql

--- server/model/data.sql
+++ server/model/data.sql
@@ -1,13 +1,13 @@
 
-INSERT INTO category(name, link) VALUES('Food, Drinks', 'food-drink');
-INSERT INTO category(name, link) VALUES('Automotive', 'automotive');
-INSERT INTO category(name, link) VALUES('Electronics', 'electronics');
-INSERT INTO category(name, link) VALUES('Fashion, Clothing', 'fashion-clothing');
-INSERT INTO category(name, link) VALUES('Floral, Garden', 'floral-garden');
-INSERT INTO category(name, link) VALUES('Health, Beauty', 'health-beauty');
-INSERT INTO category(name, link) VALUES('Home, Decor', 'home-decor');
-INSERT INTO category(name, link) VALUES('Industrial, Materials', 'industrial-materials');
-INSERT INTO category(name, link) VALUES('Pets, Animals', 'pets-animals');
-INSERT INTO category(name, link) VALUES('Sports, Outdoors', 'sports-outdoors');
-INSERT INTO category(name, link) VALUES('Toys, Games', 'toys-games');
-INSERT INTO category(name, link) VALUES('Other', 'other');
+INSERT INTO category(id, name, link) VALUES('Food, Drinks', 'food-drink');
+INSERT INTO category(id, name, link) VALUES('Automotive', 'automotive');
+INSERT INTO category(id, name, link) VALUES('Electronics', 'electronics');
+INSERT INTO category(id, name, link) VALUES('Fashion, Clothing', 'fashion-clothing');
+INSERT INTO category(id, name, link) VALUES('Floral, Garden', 'floral-garden');
+INSERT INTO category(id, name, link) VALUES('Health, Beauty', 'health-beauty');
+INSERT INTO category(id, name, link) VALUES('Home, Decor', 'home-decor');
+INSERT INTO category(id, name, link) VALUES('Industrial, Materials', 'industrial-materials');
+INSERT INTO category(id, name, link) VALUES('Pets, Animals', 'pets-animals');
+INSERT INTO category(id, name, link) VALUES('Sports, Outdoors', 'sports-outdoors');
+INSERT INTO category(id, name, link) VALUES('Toys, Games', 'toys-games');
+INSERT INTO category(id, name, link) VALUES('Other', 'other');
